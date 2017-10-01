aee2a6c55ae176a8aa22766f05cb186fe4a76568
martinak <martinak@mymail.vcu.edu>
2017-10-01 10:42:35 -0400

    Added a prototype translate function and started on the bisectSentence function. The first translates a single word and the second breaks sentences into individual words with regex and translates only the ones that meet the requirements.

--- pig.cpp
+++ pig.cpp
@@ -1,7 +1,7 @@
 /* INFO450PIG.cpp - This program takes in sentences and translates them to
-*  piglatin
-*  updated: 27 sep
-*/
+ * piglatin
+ * updated: 27 sep
+ */
 
 #include <iostream>
 #include <regex>
@@ -9,11 +9,29 @@
 
 using namespace std;
 
-void getSentence (char *sentence);
-void translate (char *sentence);
+
+void bisectSentence (char *sentence);
+void getSentence    (char *sentence);
+void translate      (char *word);
+
+
+void bisectSentence(char *sentence)
+{
+    regex wordRegex("\\w{2}\\w+");
+    cmatch matches;
+
+    regex_search(sentence, matches, wordRegex);
+
+    for(size_t i = 0; i < matches.size(); i++)
+    {
+        cout << i << " word found: " << matches[i] << " translated: " << endl;
+    }
+
+}
 
 
 void getSentence(char *sentence)
+// this funciton takes in user input and assign a pointer to it
 {
 
     char input[500]; 
@@ -24,28 +42,39 @@ void getSentence(char *sentence)
 
     strcpy(sentence, input);
     //debug
-    cout << "DEBUG: sentence = " << sentence << " *sentence = " << *sentence
-        << endl;
-    cout << "DEBUG: input = " << input << " *input = " << *input << endl;
+    //cout << "DEBUG: sentence = " << sentence << " *sentence = " << *sentence
+    //  << endl;
+    //cout << "DEBUG: input = " << input << " *input = " << *input << endl;
 }
 
 
-void translate(char *sentence)
+void translate(char *word)
 {
-    
+    char* firstLetter;
+    char ending[] = "ay";
+
+    for(int i = 1; i < strlen(word); i++)
+       cout << word[i];
+    cout << word[0] << "ay";
 }
 
-int main ( )
+
+int main ()
 {
     bool exit = false;
     char *sentence;
 
     do
     {
+        cout << "\nEntering getSentence" << endl;
         getSentence(sentence);
         // debug
-        cout << "SENTENCE = " << sentence << endl;
-        cout << "&SENTENCE = " << &sentence << endl;
-        cout << "*SENTENCE = " << *sentence << endl;
+        //cout << "SENTENCE = " << sentence << endl;
+        //cout << "&SENTENCE = " << &sentence << endl;
+        //cout << "*SENTENCE = " << *sentence << endl;
+
+        cout << "\nEntering bisectWords" << endl;
+        bisectSentence(sentence);
+
     } while( exit == false);
 }
